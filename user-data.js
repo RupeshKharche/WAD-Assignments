659e44785419ef1c0e550a8e7bf2d30b060bc119
RupeshKharche <107405035+RupeshKharche@users.noreply.github.com>
2024-01-11 15:18:30 +0530

    Delete user-data.js

--- user-data.js
+++
@@ -1,6 +0,0 @@
-// Parsing data from url query parameters
-let urlParams = new URLSearchParams(window.location.search);
-let data = urlParams.get("data");
-
-// Displaying the data
-document.getElementById("user-data-container").innerHTML = data;
